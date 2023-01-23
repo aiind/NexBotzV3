7d8faa4d76604cbb68ec1c2458462ae83efb994a
aiind <111066512+aiind@users.noreply.github.com>
2023-01-23 08:11:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -28,8 +28,8 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
 global.sig = 'https://instagram.com/itsaiind'
 global.syt = 'https://youtube.com/channel/UC28pUSJ1MyZhqGL_lamNQRw'
 global.sgh = 'https://github.com/SaepEp'
-global.sgc = 'https://chat.whatsapp.com/FZu6pDEw6pTLEE3yLQ9P3G'
-global.swa = 'wa.me/6283131688812'
+global.sgc = 'https://chat.whatsapp.com/JytIVwVnOCIFQBA93eZEXu'
+global.swa = 'wa.me/628812743180'
 global.swb = 'https://discord.gg/4HvezMAw'
 global.snh = 'https://nhentai.net/g/365296/' //Make ini aja gausah di ganti.
 
